f5f5b45359d1b155ff33022a4072c69e30c70933
Dante <maravillaalfa@gmail.com>
2024-06-26 14:25:46 -0500

    Agregar archivos de proyecto.

---
+++ SistemaVentasExamen/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace SistemaVentasExamen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtCodVendedor = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.dgvVendedor = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVendedor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(399, 227);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(354, 22);
+            this.txtNombres.TabIndex = 0;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(399, 131);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(354, 22);
+            this.txtApellidos.TabIndex = 1;
+            // 
+            // txtCodVendedor
+            // 
+            this.txtCodVendedor.Location = new System.Drawing.Point(399, 38);
+            this.txtCodVendedor.Name = "txtCodVendedor";
+            this.txtCodVendedor.Size = new System.Drawing.Size(354, 22);
+            this.txtCodVendedor.TabIndex = 2;
+            this.txtCodVendedor.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(358, 288);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(127, 39);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(500, 288);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(140, 39);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(655, 288);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(155, 39);
+            this.btnActualizar.TabIndex = 5;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // dgvVendedor
+            // 
+            this.dgvVendedor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVendedor.Location = new System.Drawing.Point(28, 333);
+            this.dgvVendedor.Name = "dgvVendedor";
+            this.dgvVendedor.RowHeadersWidth = 51;
+            this.dgvVendedor.RowTemplate.Height = 24;
+            this.dgvVendedor.Size = new System.Drawing.Size(1148, 263);
+            this.dgvVendedor.TabIndex = 6;
+            this.dgvVendedor.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1238, 636);
+            this.Controls.Add(this.dgvVendedor);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtCodVendedor);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVendedor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtCodVendedor;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.DataGridView dgvVendedor;
+    }
+}
+
---
+++ SistemaVentasExamen/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace SistemaVentasExamen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //lamar al mapeado objeto relacional a traves deun objeto
+
+        VentasDataContext ventas = new VentasDataContext();
+        private void listar()
+        {
+            var consulta = from V in ventas.Vendedor select V;
+            dgvVendedor.DataSource = consulta;
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listar();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Vendedor vendedor = new Vendedor();
+            vendedor.CodVendedor = txtCodVendedor.Text;
+            vendedor.Apellidos = txtApellidos.Text;
+            vendedor.Nombres = txtNombres.Text;
+
+            try
+            {
+                ventas.Vendedor.InsertOnSubmit(vendedor);
+                ventas.SubmitChanges();
+
+                txtCodVendedor.Clear();
+                txtApellidos.Clear();
+                txtNombres.Clear();
+                listar(); // Asegúrate de que esta función exista y haga lo que necesitas
+
+                MessageBox.Show("Registro guardado correctamente.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //  obtener el codigo vendedor que se quiere eliminar
+            var CodVendedorEliminar =(from V in ventas.Vendedor where V.CodVendedor.Contains(txtCodVendedor.Text)select V ).First();
+            ventas.Vendedor.DeleteOnSubmit(CodVendedorEliminar);
+            try
+            {
+                ventas.SubmitChanges();
+                listar();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error :" + ex.Message);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var vendedorAActualizar = (from V in ventas.Vendedor
+                                           where V.CodVendedor == txtCodVendedor.Text
+                                           select V).FirstOrDefault();
+
+                if (vendedorAActualizar != null)
+                {
+                    vendedorAActualizar.Apellidos = txtApellidos.Text;
+                    vendedorAActualizar.Nombres = txtNombres.Text;
+
+                    ventas.SubmitChanges();
+
+                    txtCodVendedor.Clear();
+                    txtApellidos.Clear();
+                    txtNombres.Clear();
+                    listar();
+
+                    MessageBox.Show("Registro actualizado correctamente.");
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró ningún vendedor con el código proporcionado.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar el registro: " + ex.Message);
+            }
+        }
+
+    }
+}
